8c427bc792cddd6a23c3dc561ab4803ceb00e6bd
Travis Crowder <travis.crowder@penton.com>
2013-10-30 14:56:14 -0500

    Working on adding Load to the index page

--- workbench/spechal/lcgp/src/views/index.blade.php
+++ workbench/spechal/lcgp/src/views/index.blade.php
@@ -6,14 +6,24 @@
 
 @section('content')
 <h4>Hosts</h4>
-<table class="table table-striped">
-@foreach($hosts as $host)
-    <tr>
-        <td><a href="/collectd/plugins/{{ $host['name'] }}">{{ $host['name'] }}</a></td>
-        <td class="@if($host['short'] > $host['cores']) alert-danger @endif">{{ round($host['short'], 2) }}</td>
-        <td class="@if($host['mid'] > $host['cores']) alert-danger @endif">{{ round($host['mid'], 2) }}</td>
-        <td class="@if($host['long'] > $host['cores']) alert-danger @endif">{{ round($host['long'], 2) }}</td>
-    </tr>
-@endforeach
+<table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <th>Host Name</th>
+            <th>Short Load</th>
+            <th>Mid Load</th>
+            <th>Long Load</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($hosts as $host)
+        <tr>
+            <td><a href="/collectd/plugins/{{ $host['name'] }}">{{ $host['name'] }}</a></td>
+            <td class="@if($host['short'] > $host['cores']) alert-danger @endif">{{ round($host['short'], 2) }}</td>
+            <td class="@if($host['mid'] > $host['cores']) alert-danger @endif">{{ round($host['mid'], 2) }}</td>
+            <td class="@if($host['long'] > $host['cores']) alert-danger @endif">{{ round($host['long'], 2) }}</td>
+        </tr>
+    @endforeach
+    </tbody>
 </table>
 @stop
