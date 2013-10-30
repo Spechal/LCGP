5e96ec5f88f9a3e9b9ffdc2466c33ae0272bafa4
Travis Crowder <travis.crowder@penton.com>
2013-10-30 12:50:54 -0500

    Adding Bootstrap DateTime Picker

--- workbench/spechal/lcgp/src/views/graph.blade.php
+++ workbench/spechal/lcgp/src/views/graph.blade.php
@@ -57,8 +57,8 @@
 <script type="text/javascript">
     <!--
         $(function(){
-            $('#start').datetimepicker();
-            $('#end').datetimepicker();
+            $('#start').datetimepicker({startDate: {{ date('m/d/Y', strtotime('3 months ago')) }}, endDate:{{ date('m/d/Y') }}});
+            $('#end').datetimepicker({endDate:{{ date('m/d/Y') }}});
         });
     //-->
 </script>
