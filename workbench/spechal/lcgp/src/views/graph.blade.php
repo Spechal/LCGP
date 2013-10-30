e1fe927201fe0e6c3cab921689f3b57c0a5ab16c
Travis Crowder <travis.crowder@penton.com>
2013-10-30 12:20:33 -0500

    Adding Bootstrap DateTime Picker

--- workbench/spechal/lcgp/src/views/graph.blade.php
+++ workbench/spechal/lcgp/src/views/graph.blade.php
@@ -15,7 +15,7 @@
 <div class="well">
     <div class="form-group">
         <div class="input-group date" id="start">
-            <input type="text" class="form-control" />
+            <input type="text" class="form-control" /><span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
         </div>
     </div>
 </div>
