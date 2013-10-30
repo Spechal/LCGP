4cb0fa698888f7730dc2bb188d18518d278a580d
Travis Crowder <travis.crowder@penton.com>
2013-10-30 14:54:17 -0500

    Working on adding Load to the index page

--- workbench/spechal/lcgp/src/views/index.blade.php
+++ workbench/spechal/lcgp/src/views/index.blade.php
@@ -10,9 +10,9 @@
 @foreach($hosts as $host)
     <tr>
         <td><a href="/collectd/plugins/{{ $host['name'] }}">{{ $host['name'] }}</a></td>
-        <td class="@if($host['short'] > $host['cores']) alert-danger @endif">{{ $host['short'] }}</td>
-        <td>{{ $host['mid'] }}</td>
-        <td>{{ $host['long'] }}</td>
+        <td class="@if($host['short'] > $host['cores']) alert-danger @endif">{{ round($host['short'], 2) }}</td>
+        <td class="@if($host['mid'] > $host['cores']) alert-danger @endif">{{ round($host['mid'], 2) }}</td>
+        <td class="@if($host['long'] > $host['cores']) alert-danger @endif">{{ round($host['long'], 2) }}</td>
     </tr>
 @endforeach
 </table>
