7af70c81ada5619f7ba60cec64e3f130fc3053c0
Travis Crowder <travis.crowder@penton.com>
2013-11-06 10:26:32 -0600

    Debugging df plugin

--- workbench/spechal/lcgp/src/Spechal/Lcgp/CollectdGraphDefault.php
+++ workbench/spechal/lcgp/src/Spechal/Lcgp/CollectdGraphDefault.php
@@ -296,6 +296,7 @@
             } else {
                 echo 'TI = '; print_r($this->_type_instances);
                 echo 'DS = '; print_r($this->_data_sources);
+                exit;
                 // source is in a single file
                 if(is_array($this->_data_sources) && count($this->_data_sources) == 1 && in_array('value', $this->_data_sources)){
                     $sources = $this->_type_instances;
