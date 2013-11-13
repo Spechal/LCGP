ddcbb35a326c0c9c9edb54fbd38b84a859d29c83
Travis Crowder <travis.crowder@penton.com>
2013-11-13 09:28:10 -0600

    Working on adding a search box to the main page

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,7 +1,43 @@
-jQuery.extend({highlight:function(a,b,c,d){if(a.nodeType===3){var e=a.data.match(b);if(e){var f=document.createElement(c||"span");f.className=d||"highlight";var g=a.splitText(e.index);g.splitText(e[0].length);var h=g.cloneNode(true);f.appendChild(h);g.parentNode.replaceChild(f,g);return 1}}else if(a.nodeType===1&&a.childNodes&&!/(script|style)/i.test(a.tagName)&&!(a.tagName===c.toUpperCase()&&a.className===d)){for(var i=0;i<a.childNodes.length;i++){i+=jQuery.highlight(a.childNodes[i],b,c,d)}}return 0}});jQuery.fn.unhighlight=function(a){var b={className:"highlight",element:"span"};jQuery.extend(b,a);return this.find(b.element+"."+b.className).each(function(){var a=this.parentNode;a.replaceChild(this.firstChild,this);a.normalize()}).end()};jQuery.fn.highlight=function(a,b){var c={className:"highlight",element:"span",caseSensitive:false,wordsOnly:false};jQuery.extend(c,b);if(a.constructor===String){a=[a]}a=jQuery.grep(a,function(a,b){return a!=""});a=jQuery.map(a,function(a,b){return a.replace(/[-[\]{}()*+?.,\\^$|#\s]/g,"\\$&")});if(a.length==0){return this}var d=c.caseSensitive?"":"i";var e="("+a.join("|")+")";if(c.wordsOnly){e="\\b"+e+"\\b"}var f=new RegExp(e,d);return this.each(function(){jQuery.highlight(this,f,c.element,c.className)})}
+jQuery.fn.highlight = function(pat) {
+    function innerHighlight(node, pat) {
+        var skip = 0;
+        if (node.nodeType == 3) {
+            var pos = node.data.toUpperCase().indexOf(pat);
+            if (pos >= 0) {
+                var spannode = document.createElement('span');
+                spannode.className = 'highlight';
+                var middlebit = node.splitText(pos);
+                var endbit = middlebit.splitText(pat.length);
+                var middleclone = middlebit.cloneNode(true);
+                spannode.appendChild(middleclone);
+                middlebit.parentNode.replaceChild(spannode, middlebit);
+                skip = 1;
+            }
+        }
+        else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+            for (var i = 0; i < node.childNodes.length; ++i) {
+                i += innerHighlight(node.childNodes[i], pat);
+            }
+        }
+        return skip;
+    }
+    return this.length && pat && pat.length ? this.each(function() {
+        innerHighlight(this, pat.toUpperCase());
+    }) : this;
+};
+
+jQuery.fn.removeHighlight = function() {
+    return this.find("span.highlight").each(function() {
+        this.parentNode.firstChild.nodeName;
+        with (this.parentNode) {
+            replaceChild(this.firstChild, this);
+            normalize();
+        }
+    }).end();
+};
 
 $(document).ready(function(){
-alert('running');
+
 // Config
     var SearchBox = "#search-box";
     var SearchableElements = "#searchable blockquote";
@@ -16,7 +52,7 @@ alert('running');
 
         var filter = $(this).val();
         var count = 0;
-        $('#searchable').unhighlight()
+        $('#searchable').removeHighlight()
 
 
         $(SearchableElements).each(function() {
@@ -31,7 +67,7 @@ alert('running');
             } else {
                 $(this).parent().parent().animate({
                     opacity: 1,
-                    height: '100%',
+                    height: '100%'
                 }, 100, function() {
                     $(this).show(100);
                 });
